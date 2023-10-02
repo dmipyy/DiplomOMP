4a5ed9857dd198790ef0ed05bf2165287818b411
dmipyy <reyper2014@yandex.ru>
2023-10-02 21:30:03 +0300

    рефакторинг реализации циклического буффера

--- PipeDriver/circBuff.c
+++ PipeDriver/circBuff.c
@@ -12,6 +12,7 @@ STATUS CircBuffInit(void* buf, uint16_t size, size_t cellSize, CircBuff_t *cb)
 STATUS CircBuffReset(CircBuff_t *cb)
 {
 	if (cb->buf == NULL) return PARAM_ERR; //если буфера нет - ошибка
+	
 	cb->head = 0;
 	cb->tail = 0
 	return OK;
@@ -20,42 +21,46 @@ STATUS CircBuffReset(CircBuff_t *cb)
 STATUS CircBuffBytePut(const uint8_t data, CircBuff_t *cb)
 {
 	if(cb->buf == NULL) return PARAM_ERR;
+	
 	cb->buf[cb->head++] = data;
 	if (cb->head >= cb->size)//если голова вышла за пределы буфера
 		cb->head = 0;		 //перещаем ее на начало буфера
 	return OK;
 }
 
-STATUS STATUS CircBuffCellPut(const void *data, CircBuff_t *cb); {
-    return CircBuffDataPut(data, 1, rb);
+STATUS CircBuffCellPut(const void *data, CircBuff_t *cb);
+{
+    return CircBuffDataPut(data, 1, cb);
 }
 
-
-RINGBUF_STATUS RingBuf_DataPut(const void *data, u16_t len, RINGBUF_t *rb) {
-    if (rb->buf == NULL) return RINGBUF_PARAM_ERR;
-    if (len > rb->size)
-        return RINGBUF_OVERFLOW;
+STATUS CircBuffDataPut(const void *data, uint16_t len, CircBuff_t *cb)
+{
+    if (cb->buf == NULL) return PARAM_ERR;
+    if (len > cb->size)
+        return OVERFLOW;
+	
     const char *input = data; // recast pointer
     // INPUT data index start address
-    size_t s_addr = 0;
+    size_t startAddress = 0;
     // available space in the end of buffer
-    size_t space = rb->size - rb->head;
-    if (len > space) { // if len > available space
+    size_t space = cb->size - cb->head;
+    if (len > space)
+	{ // if len > available space
         // copy data to available space
-        memcpy(&rb->buf[rb->head*rb->cell_size], &input[s_addr * rb->cell_size], space * rb->cell_size);
+        memcpy(&cb->buf[cb->head*cb->cellSize], &input[startAddress * cb->cellSize], space * cb->cellSize);
         // next writing will start from 0
-        rb->head = 0;
+        cb->head = 0;
         // new start address = space length
-        s_addr = space;
+        startAddress = space;
         // new length = len-space
         len -= space;
     }
     // copy all the data to the buf storage
-    memcpy(&rb->buf[rb->head*rb->cell_size], &input[s_addr * rb->cell_size], len * rb->cell_size);
+    memcpy(&cb->buf[cb->head*cb->cellSize], &input[startAddress * cb->cellSize], len * cb->cellSize);
     // shift to the next head
-    rb->head += len;
-    if (rb->head >= rb->size)
-        rb->head = 0;
+    cb->head += len;
+    if (cb->head >= cb->size)
+        cb->head = 0;
     return RINGBUF_OK;
 }
 
@@ -63,28 +68,31 @@ RINGBUF_STATUS RingBuf_DataPut(const void *data, u16_t len, RINGBUF_t *rb) {
  * @brief Read next byte from the buffer
  *
  * @param[out] data Data byte from the buffer
- * @param[in] rb #RINGBUF_t structure instance
- * @return #RINGBUF_STATUS enum
+ * @param[in] cb #RINGBUF_t structure instance
+ * @return #STATUS enum
  */
-RINGBUF_STATUS RingBuf_ByteRead(u8_t *data, RINGBUF_t *rb) {
-    if (rb->buf == NULL) return RINGBUF_PARAM_ERR;
-    RINGBUF_STATUS st = RingBuf_ByteWatch(data, rb);
-    if (st != RINGBUF_OK)
+STATUS CircBuffByteRead(uint8_t *data, CircBuff_t *cb)
+{
+    if (cb->buf == NULL) return PARAM_ERR;
+	
+    STATUS st = CircBuffByteWatch(data, cb);
+    if (st != OK)
         return st;
-    rb->tail++;
-    if (rb->tail >= rb->size)
-        rb->tail = 0;
+    cb->tail++;
+    if (cb->tail >= cb->size)
+        cb->tail = 0;
     return st;
 }
 
 /**
  * @brief Read 1 cell from buf
  * @param[out] data Data cell from the buffer
- * @param[in] rb #RINGBUF_t structure instance
- * @return #RINGBUF_STATUS enum
+ * @param[in] cb #RINGBUF_t structure instance
+ * @return #STATUS enum
  */
-RINGBUF_STATUS RingBuf_CellRead(void *data, RINGBUF_t *rb) {
-    return RingBuf_DataRead(data, 1, rb);
+STATUS CircBuffCellRead(void *data, CircBuff_t *cb)
+{
+    return CircBuffDataRead(data, 1, cb);
 }
 
 /**
@@ -92,19 +100,21 @@ RINGBUF_STATUS RingBuf_CellRead(void *data, RINGBUF_t *rb) {
  *
  * @param[out] data Data from the buffer
  * @param[in] len Length of data to be read [bytes]
- * @param[in] rb #RINGBUF_t structure instance
- * @return #RINGBUF_STATUS enum
+ * @param[in] cb #RINGBUF_t structure instance
+ * @return #STATUS enum
  */
-RINGBUF_STATUS RingBuf_DataRead(void *data, u16_t len, RINGBUF_t *rb) {
-    if (rb->buf == NULL) return RINGBUF_PARAM_ERR;
+STATUS CircBuffDataRead(void *data, uint16_t len, CircBuff_t *cb)
+{
+    if (cb->buf == NULL) return PARAM_ERR;
+	
     // read data
-    RINGBUF_STATUS st = RingBuf_DataWatch(data, len, rb);
-    if (st != RINGBUF_OK)
+    STATUS st = CircBuffDataWatch(data, len, cb);
+    if (st != OK)
         return st;
     // shift to the next head
-    rb->tail += len;
-    if (rb->tail >= rb->size)
-        rb->tail = 0;
+    cb->tail += len;
+    if (cb->tail >= cb->size)
+        cb->tail = 0;
     return st;
 }
 
@@ -113,13 +123,14 @@ RINGBUF_STATUS RingBuf_DataRead(void *data, u16_t len, RINGBUF_t *rb) {
  * @note Reads data without shifting in the buffer
  *
  * @param[out] data Pointer to data byte got from buffer
- * @param[in] rb #RINGBUF_t structure instance
- * @return #RINGBUF_STATUS enum
+ * @param[in] cb #RINGBUF_t structure instance
+ * @return #STATUS enum
  */
-RINGBUF_STATUS RingBuf_ByteWatch(u8_t *data, RINGBUF_t *rb) {
-    if (data == NULL) return RINGBUF_PARAM_ERR;
-    *data = rb->buf[rb->tail];
-    return RINGBUF_OK;
+STATUS CircBuffByteWatch(uint8_t *data, CircBuff_t *cb)
+{
+    if (data == NULL) return PARAM_ERR;
+    *data = cb->buf[cb->tail];
+    return OK;
 }
 
 /**
@@ -128,10 +139,11 @@ RINGBUF_STATUS RingBuf_ByteWatch(u8_t *data, RINGBUF_t *rb) {
  *
  * @param[out] data Pointer to data cell got from buffer
  * @param[in] #RINGBUF_t structure instance
- * @return #RINGBUF_STATUS enum
+ * @return #STATUS enum
  */
-RINGBUF_STATUS RingBuf_CellWatch(void *data, RINGBUF_t *rb) {
-    return RingBuf_DataWatch(data, 1, rb);
+STATUS CircBuffCellWatch(void *data, CircBuff_t *cb)
+{
+    return CircBuffDataWatch(data, 1, cb);
 }
 
 /**
@@ -140,33 +152,36 @@ RINGBUF_STATUS RingBuf_CellWatch(void *data, RINGBUF_t *rb) {
  *
  * @param[out] data Data from buffer
  * @param[in] len Length of data to be read [bytes]
- * @param[in] rb #RINGBUF_t structure instance
- * @return #RINGBUF_STATUS enum
+ * @param[in] cb #RINGBUF_t structure instance
+ * @return #STATUS enum
  */
-RINGBUF_STATUS RingBuf_DataWatch(void *data, u16_t len, RINGBUF_t *rb) {
+STATUS CircBuffDataWatch(void *data, uint16_t len, CircBuff_t *cb)
+{
     if (data == NULL)
-        return RINGBUF_PARAM_ERR;
-    if (len > rb->size)
-        return RINGBUF_OVERFLOW;
+        return PARAM_ERR;
+    if (len > cb->size)
+        return OVERFLOW;
+	
     // OUTPUT data index start address
-    u16_t s_addr = 0;
+    uint16_t startAddress = 0;
     // available space in the end of buffer
-    u16_t space = rb->size - rb->tail;
-    u16_t loc_tail = rb->tail;
-    if (len > space) { // if len > available space
-        // recast pointer to u8_t
+    uint16_t space = cb->size - cb->tail;
+    uint16_t loc_tail = cb->tail;
+    if (len > space)
+	{ // if len > available space
+        // recast pointer to uint8_t
         // copy data from available space
-        memcpy(&data[s_addr * rb->cell_size], &rb->buf[loc_tail * rb->cell_size], space * rb->cell_size);
+        memcpy(&data[startAddress * cb->cellSize], &cb->buf[loc_tail * cb->cellSize], space * cb->cellSize);
         // next reading will start from 0
         loc_tail = 0;
         // new start address - space length
-        s_addr = space;
+        startAddress = space;
         // new length - len-space
         len -= space;
     }
     // copy all the data from the buf storage
-    memcpy(&data[s_addr * rb->cell_size], &rb->buf[loc_tail * rb->cell_size], len * rb->cell_size);
-    return RINGBUF_OK;
+    memcpy(&data[startAddress * cb->cellSize], &cb->buf[loc_tail * cb->cellSize], len * cb->cellSize);
+    return OK;
 }
 
 
